892d0a9e3ea34566d59d9fcccfe9a756410d52f5
oviya <ooviyanagarajan@yahoo.com>
2018-12-26 22:29:40 +0530

    Commiting

--- app/src/main/java/com/example/guru/databaseapplication/MainActivity.java
+++ app/src/main/java/com/example/guru/databaseapplication/MainActivity.java
@@ -7,53 +7,79 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.IOException;
+
 
 public class MainActivity extends Activity{
     EditText et_nme,et_sid,et_ml;
-    Button b1;
+    Button btnshow, btnupd, btndel, btnreg;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         et_nme = (EditText)findViewById(R.id.studname);
-      et_sid = (EditText)findViewById(R.id.studid);
-      et_ml = (EditText)findViewById(R.id.email);
-        b1=(Button)findViewById(R.id.btnshow);
+        et_sid = (EditText)findViewById(R.id.studid);
+        et_ml = (EditText)findViewById(R.id.email);
+
+
+
+        btnshow=(Button)findViewById(R.id.btnshow);
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ActivityDetails.class));
+
+            }
+        });
+
+
+
+        btnupd=(Button)findViewById(R.id.button);
+        btnupd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num=et_sid.getText().toString();
+                String mail=et_ml.getText().toString();
+                String method="update";
+                Processing p=new Processing(MainActivity.this);
+                p.execute(method,num,mail);
+            }
+        });
+
+        btndel=(Button)findViewById(R.id.button2);
+        btndel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num=et_sid.getText().toString();
+                String method="delete";
+                Processing p=new Processing(MainActivity.this);
+                p.execute(method,num);
+            }
+        });
+
+
+        btnreg=(Button)findViewById(R.id.btnreg);
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=et_nme.getText().toString();
+                String num=et_sid.getText().toString();
+                String mail=et_ml.getText().toString();
+                String method="register";
+                Processing p=new Processing(MainActivity.this);
+                p.execute(method,name,num,mail);
+
+            }
+        });
 
 
     }
 
-    public void StudReg(View view) {
 
-        String name=et_nme.getText().toString();
-        String num=et_sid.getText().toString();
-        String mail=et_ml.getText().toString();
 
-        String method="register";
-
-        Processing p=new Processing(this);
-        p.execute(method,name,num,mail);
-
-    }
-
-    public void StudDelete(View view) {
-
-        String delid=et_sid.getText().toString();
-        String method="delete";
-        Processing p=new Processing(this);
-        p.execute(method,delid);
-
-    }
-
-    public void StudUpdate(View view) {
-        String upid=et_sid.getText().toString();
-        String method="update";
-        Processing p=new Processing(this);
-        p.execute(method,upid);
-    }
-    public void showdet(View b1) {
-
-        startActivity(new Intent(this,ActivityDetails.class));
-    }
 }
