d70eb519904c69f5c54d21871bf8da08572c8a01
oviya <ooviyanagarajan@yahoo.com>
2018-12-26 22:20:59 +0530

    Commiting

--- app/src/main/java/com/example/guru/databaseapplication/Processing.java
+++ app/src/main/java/com/example/guru/databaseapplication/Processing.java
@@ -33,23 +33,8 @@ public class Processing extends AsyncTask<String,Void,String> {
 
     @Override
     protected void onPostExecute(String res) {
-        if(res.equals("Registration Success..."))
-        {
-            Toast.makeText(ctx, res, Toast.LENGTH_LONG).show();
-        }
-        else if(res.equals("Deleted Successfully"))
-        {
-            Toast.makeText(ctx, res, Toast.LENGTH_LONG).show();
-        }
-        else if(res.equals("Updated Successfully"))
-        {
-            Toast.makeText(ctx, res, Toast.LENGTH_LONG).show();
-        }
-        else
-        {
             alertDialog.setMessage(res);
             alertDialog.show();
-        }
 
     }
 
@@ -71,6 +56,8 @@ public class Processing extends AsyncTask<String,Void,String> {
 
               try {
                   URL url = new URL(reg_url);
+
+
                   HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                   httpURLConnection.setRequestMethod("POST");
                   httpURLConnection.setDoOutput(true);
@@ -86,7 +73,7 @@ public class Processing extends AsyncTask<String,Void,String> {
                   OS.close();
                   InputStream IS = httpURLConnection.getInputStream();
                   IS.close();
-                  //httpURLConnection.connect();
+
                   httpURLConnection.disconnect();
                   return "Registration Success...";
 
@@ -98,34 +85,83 @@ public class Processing extends AsyncTask<String,Void,String> {
               }
           }
 
-          else if(method.equals("delete")) {
-              String id = params[1];
+           if(method.equals("delete")) {
+               String studid = params[1];
 
               try {
                   URL url2 = new URL(del_url);
                   HttpURLConnection httpURLConnection = (HttpURLConnection) url2.openConnection();
-                  httpURLConnection.setRequestMethod("POST");
+                //  httpURLConnection.setRequestMethod("GET");
                   httpURLConnection.setDoOutput(true);
                   httpURLConnection.setDoInput(true);
+
+                  OutputStream OS = httpURLConnection.getOutputStream();
+                  BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                  String data = URLEncoder.encode("studid", "UTF-8") + "=" + URLEncoder.encode(studid, "UTF-8");
+                  bufferedWriter.write(data);
+                  bufferedWriter.flush();
+                  bufferedWriter.close();
+                  OS.close();
+
+                  InputStream inputStream = httpURLConnection.getInputStream();
+                  BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                  String response = "";
+                  String line = "";
+                  while ((line = bufferedReader.readLine())!=null)
+                  {
+                      response+= line;
+                  }
+                  bufferedReader.close();
+                  inputStream.close();
                   httpURLConnection.disconnect();
-                  return "Deleted Successfully";
+                  return response;
+
+
               } catch (MalformedURLException e) {
                 result = e.getMessage();
                 } catch (IOException e) {
                  result = e.getMessage();
                 }
           }
-          else if(method.equals("update")) {
-              String id = params[1];
+           if (method.equals("update")) {
+              String studid = params[1];
+              String email = params[2];
 
               try {
                   URL url2 = new URL(update_url);
+
                   HttpURLConnection httpURLConnection = (HttpURLConnection) url2.openConnection();
-                  httpURLConnection.setRequestMethod("POST");
+                 // httpURLConnection.setRequestMethod("POST");
                   httpURLConnection.setDoOutput(true);
                   httpURLConnection.setDoInput(true);
+
+
+                  OutputStream OS = httpURLConnection.getOutputStream();
+
+                  BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                  String data = URLEncoder.encode("studid", "UTF-8") + "=" + URLEncoder.encode(studid, "UTF-8") + "&" +
+                          URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+                  bufferedWriter.write(data);
+                  bufferedWriter.flush();
+                  bufferedWriter.close();
+                  OS.close();
+
+                  InputStream inputStream = httpURLConnection.getInputStream();
+                  BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                  String response = "Updated Successfully";
+                  String line = "";
+                  while ((line = bufferedReader.readLine())!=null)
+                  {
+                      response+= line;
+                  }
+                  bufferedReader.close();
+                  inputStream.close();
                   httpURLConnection.disconnect();
-                  return "Updated Successfully";
+                  return response;
+
+
+
+
               } catch (MalformedURLException e) {
                   result = e.getMessage();
               } catch (IOException e) {
